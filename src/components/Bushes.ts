5e936f70fcbcd183db50d73253ed38df618ed89a
Pomidorchics <aleksandra180504@gmail.com>
2024-01-29 16:36:06 +1000

    bushes

---
+++ src/components/Bushes.ts
@@ -0,0 +1,14 @@
+class Bushes {
+    x: number;
+    y: number;
+    img: string;
+    constructor(x: number, y: number, img: string) {
+        this.x = x;
+        this.y = y;
+        this.img = img;
+    }
+
+    move() {
+        console.log("HelloWorld")
+    }
+}
