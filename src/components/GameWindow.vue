296f1b46c1698ce4803a3a222a122de8c3a5cad4
Pomidorchics <aleksandra180504@gmail.com>
2024-01-29 15:29:32 +1000

    bush

--- src/components/GameWindow.vue
+++ src/components/GameWindow.vue
@@ -5,15 +5,28 @@
 </template>
 
 <script>
+
     export default {
         mounted: function() {
             const canvas = this.$refs.supercanvas;
             const ctx = canvas.getContext('2d');
-            const pic = new Image();
-            pic.src = require('/src/assets/cat.png');
-            pic.onload = function() {
-                ctx.drawImage(pic, 0, 0, 800, 600);
-            }
+            // const pic = new Image();
+            // pic.src = require('/src/assets/cat.png');
+            // pic.onload = function() {
+            //     ctx.drawImage(pic, 0, 0, 800, 600);
+            // }
+            ctx.fillRect(100, 100, 50, 50);
+            
+            var x = 750;
+            var y = 550;
+
+            ctx.fillRect(x, y, 50, 50);
+
+            setInterval(() => {
+                ctx.clearRect(x, y, 50, 50)
+                x--;
+                ctx.fillRect(x, y, 50, 50);
+            }, 10);
         }
     }
 </script>
