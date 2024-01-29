dd743d561f1e5d389cef1b90f14ea3d2ea925d82
Muzychenko Alevtina <fghbaim808@gmail.com>
2024-01-29 15:33:16 +1000

    autumn png

--- src/components/GameWindow.vue
+++ src/components/GameWindow.vue
@@ -5,12 +5,14 @@
 </template>
 
 <script>
+    const x = 750;
+    const y = 550;
     export default {
         mounted: function() {
             const canvas = this.$refs.supercanvas;
             const ctx = canvas.getContext('2d');
             const pic = new Image();
-            pic.src = require('/src/assets/cat.png');
+            pic.src = require('/src/assets/autumn.png');
             pic.onload = function() {
                 ctx.drawImage(pic, 0, 0, 800, 600);
             }
